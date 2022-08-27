2489849dcaf9f10f30beb8be22c72bc2506cb651
iranmilad <kazemi.milad@gmail.com>
2022-08-28 03:02:25 +0430

    1

--- TrendCalculator.py
+++ TrendCalculator.py
@@ -445,7 +445,7 @@ def now_time_run():
     time_tset_now = now.strftime("%H%M")
     weekday=datetime.today().weekday()
     print(time_tset_now)
-    s_t = "1701"
+    s_t = "0301"
     e_t = "1702"
     if time_tset_now <e_t and time_tset_now >= s_t and weekday in [6,5,0,1,2]:
         exit_run = False
